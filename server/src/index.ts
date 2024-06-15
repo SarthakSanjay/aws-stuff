650617758c90e5d7d82e81a9aaf58d225736a913
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-06-15 23:57:15 +0530

    added function to get object url

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,23 @@
-import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { GetObjectCommand, PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { ACCESS_KEY, SECRET_KEY } from './secret';
 
 const s3client = new S3Client({
     region: "ap-south-1",
     credentials:{
-        accessKeyId:'',
-        secretAccessKey:''
+        accessKeyId: ACCESS_KEY,
+        secretAccessKey: SECRET_KEY
     }
 })
+
+async function getObjectUrl(key:any){
+    const command = new GetObjectCommand({
+        Bucket: 'sharko-bucket',
+        Key: key
+    })
+    const url = await getSignedUrl(s3client ,command)
+    return url
+}
 
 async function putObject(filename:string , contentType: string) {
     const command = new PutObjectCommand({
@@ -22,7 +32,9 @@ async function putObject(filename:string , contentType: string) {
 }
 
 async function init() {
-    console.log(`
-    url for upoloading
-    `)
+    console.log('main function');
+    const signedUrl = await getObjectUrl("ch.png")
+    console.log(signedUrl);
 }
+
+init()
